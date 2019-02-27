5ee0b13fcf20e24948ab12c30987f9cb1ad62937
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2019-02-27 12:08:49 +0000

    Change message from Goodbye to Hello

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__, instance_relative_config=True)
 
 @app.route('/')
 def hello_world():
-    recipient = os.getenv("RECIPIENT", "world")
+    recipient = os.getenv("RECEIVER", "unknown")
 
-    return jsonify({"message": "Goodbye", "recipient": recipient})
+    return jsonify({"message": "Hello", "recipient": recipient, "result": "OK"})
 
 if __name__ == "__main__":
     app.run()
